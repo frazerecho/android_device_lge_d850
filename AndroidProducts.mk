5ed9109f15d9a3083d5391176b74aa81bea431cc
Hasan Okarvi <frazerecho@users.noreply.github.com>
2018-10-20 16:46:57 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_d850.mk
+    $(LOCAL_DIR)/havoc_d850.mk
